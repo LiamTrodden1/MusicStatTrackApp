72b6ba6a1ac9ddd4d019999676c3eea8c391d0f3
LiamTrodden1 <liamtrodden28@gmail.com>
2025-10-07 11:25:35 +0100

    Page Naviagtion

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 console.log("User Logged");
 
+// temp Values
+const albums = [
+    { title: "AM", artist: "Arctic Monkeys" }
+];
+
+// Calculate total
+const totalAlbums = albums.length;
+
+// display on page
+document.getElementById("totalAlbums").textContent = totalAlbums;
+
